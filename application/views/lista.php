b8775cf3e2d1e38afa86ac405eb895010c64d2a8
andrew <andrew.ermel@gmail.com>
2020-09-11 13:24:15 -0300

    active estilizado

--- application/views/lista.php
+++ application/views/lista.php
@@ -7,15 +7,15 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body >
-    <div class ='container'>
+    <div class ='container'  >
         <h1>Lista de Compras</h1>
-        <div>
+        <div class="list-group">
             <?php foreach($Items as $item) {?>
-                <?=$item->nome?><?=$item->done?>
-                <a href=del-rota?id=<?=$item->id?>>x</a>
-                <a href=done-rota?id=<?=$item->id?>&done=<?=$item->done?>>v</a>
-                
-                <br>
+                <div class="list-group-item <?php If($item->done == 1){echo 'active';}?>" ><?=$item->nome?>                                           
+                <a href=del-rota?id=<?=$item->id?> class="close" >x</a>
+                <a href=done-rota?id=<?=$item->id?>&done=<?=$item->done?> class="close" >v</a>
+                </div> 
+              
             <?php }?>
         </div>
 
