98f72ca69a930acd8d53ebb35b29542388f6be81
andrew <andrew.ermel@gmail.com>
2020-09-11 19:44:44 -0300

    validando entradas de usuarios

--- application/controllers/AddController.php
+++ application/controllers/AddController.php
@@ -8,12 +8,24 @@ class AddController extends CI_Controller {
         public function addItem(){
                 $nome= $_GET['nomeItem'];
 
+                if(empty($nome)){
+                        echo "nome do item não pode ser em branco";
+                        echo '<a href="/" > voltar </a>';
+                        }elseif(is_numeric($nome)){
+                                echo "nome do item não pode ser um numero";
+                                echo '<a href="/" > voltar </a>';
+
+                        }else {
+                                $this->load->model('Item_model');
+                                $this->Item_model->addItems($nome);
+                                header('location:/');
+                        }
+
                 
 
-                $this->load->model('Item_model');
-                $this->Item_model->addItems($nome);
+            
 
-                header('location:/');
+               
 
         }
 
